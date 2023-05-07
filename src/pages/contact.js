028ab3f31cbb3393679df92fe02466f3aa3e1ad8
Leonardo Freitas <leonardofreitasdev@gmail.com>
2023-05-07 08:32:01 -0300

    first commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
-export default function Index() {
+export default function Contact() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-9xl">Hello world</h1>
+      <h1 className="text-9xl">Contact</h1>
     </div>
   )
 }
